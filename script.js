2146ad205405439bf63db71589f8ad82428a0dda
Pavlo <pmatsibokh@gmail.com>
2016-06-16 21:18:25 +0000

    initial

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 function play(){
-	var size= 100; //розмір ячейок
-	//масив для рандомного перемішання кольорів:
+	var size= 100; //cell size
+	//random color array
 	var arr = ['#FF0000','#FF0000','#FF009E','#FF009E','#9100FF','#9100FF','#0000FF','#0000FF','#00FFFC','#00FFFC','#09FF00','#09FF00','#E2FF00','#E2FF00','#4B4646','#4B4646'];
-	//масив з кольорами для ячейок
+	//cell color array
 	var e = [['#FF0000','#FF0000','#FF009E','#FF009E'],['#9100FF','#9100FF','#0000FF','#0000FF'],['#00FFFC','#00FFFC','#09FF00','#09FF00'],['#E2FF00','#E2FF00','#4B4646','#4B4646']];
-	var n=0;  //початкове значення для ключів масиву arr
-	var fin=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]; //початковий масив для перевірки того, чи відкрита ячейка (по замовчуванню всі зариті)
-	var win=0; //початкове значення для відкритих пар кольорів
-	var k=0; // кількість кліків (до 2-х)
-	var color1=null;
-	var color2=null;
-	var detailsOneX=null;
-	var detailsOneY=null;
-	var detailsTwoX=null;
-	var detailsTwoY=null;
+	var n=0;  //initial arr key value
+	var fin=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]; //initial cell array
+	var win=0; //winner counter
+	var k=0; // click counter
+	var isTime = true; // result time out flag
+ 	var color1, color2, detailsOneX, detailsOneY, detailsTwoX, detailsTwoY;
 	
-	//малюємо пусту дошку. 
-	var bord = document.getElementById('bord');
-	var ctx = bord.getContext('2d');
-	ctx.fillRect(0,0,bord.width,bord.height);
+	//draw empty board 
+	var board = document.getElementById('board');
+	var ctx = board.getContext('2d');
+	init();
+	ctx.fillRect(0,0,board.width,board.height);
 	
-		//малюєм сірі ячейки
+		//fill up board
 		for (var i=0; i<4; i++){
 			for (var j=0; j<4; j++){
 				ctx.fillStyle='#D3D3D3';
@@ -28,29 +25,32 @@ function play(){
 			}	
 		}
 	
-	
-	this.getRandom = function(a,b){
+	//random color mix
+	function getRandom(a,b){
 		return Math.random()-0.5;
 	};
-	var arr1=arr.sort(getRandom);
-		ctx.fillStyle= arr1[n];
-		ctx.fillRect(i*size, j*size, 99, 99);
-	for (i=0;i<4;i++){
-		for(j=0;j<4;j++){
-			e[i][j]=arr1[n]
-			n++
-		}
-	};
+	function init(){
+		var arr1=arr.sort(getRandom);
+		for (i=0;i<4;i++){
+			for(j=0;j<4;j++){
+				e[i][j]=arr1[n]
+				n++
+			}
+		};
+	}
 		
+	// winner verification
 	function checkRes(){
 		if (win===8){
-			alert("You Win");
+			alert("Congratulation! You are winner");
 		}
 	};
 	
+	//first click handler
 	function eventOne(x, y) { 
 		if (fin[x][y]===1){
-			alert("Ця ячейка вже відкрита");
+			alert("This color is already match");
+			isTime=true;
 		} else if (fin[x][y]===0){
 			ctx.fillStyle= e[y][x];
 			ctx.fillRect(y*size, x*size, 99, 99);
@@ -62,9 +62,11 @@ function play(){
 		}
 	};
 
+	//second click handler
 	function eventTwo(x, y, detailsOneX, detailsOneY){
 		if (fin[x][y]===1 || (detailsOneX===x && detailsOneY===y) ){
-			alert("Ця ячейка вже відкрита");
+			alert("This color is already match");
+			isTime=true;
 		} else if (fin[x][y]===0){
 			ctx.fillStyle= e[y][x];
 			ctx.fillRect(y*size, x*size, 99, 99);
@@ -77,25 +79,32 @@ function play(){
 		}
 	};
 
+	//color matching verification
 	function result(color1,color2,detailsOneX,detailsOneY, detailsTwoX,detailsTwoY){
 		if (color1===color2){
 			fin[detailsOneX][detailsOneY]=1;
 			fin[detailsTwoX][detailsTwoY]=1;
 			win++;
 			checkRes();
-		} else if (color1!==color2)
+		} else if (color1!==color2) {
 			ctx.fillStyle='#D3D3D3';
 			ctx.fillRect(detailsOneY*size, detailsOneX*size, 99, 99);
 			ctx.fillRect(detailsTwoY*size, detailsTwoX*size, 99, 99);
+		}
+		isTime=true;
 	};
 
-	bord.onclick=function(e){
-		var y = Math. floor((e.pageX - bord.offsetLeft)/100);
-		var x = Math. floor((e.pageY - bord.offsetTop)/100);
-		if(k===0){
-			eventOne(x, y);
-		} else if ((k===1)){
-			eventTwo(x, y, detailsOneX, detailsOneY);
+	//on board click handler
+	board.onclick=function(e){
+		if(isTime){
+			var y = Math. floor((e.pageX - board.offsetLeft)/100);
+			var x = Math. floor((e.pageY - board.offsetTop)/100);
+			if(k===0){
+				eventOne(x, y);
+			} else if ((k===1)){
+				isTime=false;
+				eventTwo(x, y, detailsOneX, detailsOneY);
+			}
 		}
 	};
 }
